15110368f9e427aa6c5fa7650a559fc1d80733c8
Lavish Bansal <lavish.bansal.mat19@iitbhu.ac.in>
2021-03-31 15:44:22 +0530

    model_plot added

---
+++ plot.py
@@ -0,0 +1,14 @@
+import sys
+path = sys.path[0]
+sys.path.insert(1, path + '\\engine')
+import tensorflow as tf
+
+from model import VAE
+
+latent_dim = 2
+img_shape = (28,28,1)
+model = VAE(img_shape, latent_dim)
+model.built = True
+
+tf.keras.utils.plot_model(model.encoder, to_file = "images/encoder.jpg", show_shapes=True)
+tf.keras.utils.plot_model(model.decoder, to_file = "images/decoder.jpg", show_shapes = True)
